b654857f9998b6d6e56406f7879ea2733749a586
Philipp323 <philipp-auinger@gmx.at>
2019-06-16 09:36:41 +0200

    Tooltip bei Auswahl eines anderen Stockwerkes verschwinden lassen

--- Application/js/own/selectObjects.js
+++ Application/js/own/selectObjects.js
@@ -1,5 +1,4 @@
 async function onDocumentMouseDown(event) {
-
     event.preventDefault();
 
     mouse3D = new THREE.Vector3(
@@ -9,15 +8,40 @@ async function onDocumentMouseDown(event) {
     var raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(mouse3D, camera);
 
-    intersectObjects = raycaster.intersectObjects(roomObjects, true);
+    var helpChar;
+    if (floors[0].toString() == visableFloorName) {
+      helpChar = "U";
+    }
+    if (floors[1].toString() == visableFloorName) {
+      helpChar = "E";
+    }
+    if (floors[2].toString() == visableFloorName) {
+      helpChar = "1";
+    }
+    if (floors[3].toString() == visableFloorName) {
+      helpChar = "2";
+    }
+    visableRooms = objectArr.filter(
+      //filter visable(clickable)Rooms, Floors shouldn't be there.
+      x =>
+        x.name[0] == helpChar &&
+        x.name != floors[0] &&
+        x.name != floors[1] &&
+        x.name != floors[2] &&
+        x.name != floors[3]
+    );
+
+    console.log(visableRooms);
+
+    intersectObjects = raycaster.intersectObjects(visableRooms, true);
+    intersectedFloors = raycaster.intersectObjects(objects, true);
+
     //intersectInfopoints = raycaster.intersectObjects(YOUR_CLICKABLE_INFOPOINTS, true);
     console.log("Entered Mouse-Down");
-    if (intersectObjects.length > 0) {
+    if (intersectObjects.length > 0 && MODE == "ROOM") {
         latestMouseIntersection = intersectObjects[0].point;
 
-        if(selectedRoom != undefined){
-            selectedRoom.material.color = white;
-        }
+        hideTooltip();
 
         selectedRoom = intersectObjects[0].object;
         randomColor = new THREE.Color(Math.random() * 0xffffff);
@@ -25,5 +49,12 @@ async function onDocumentMouseDown(event) {
 
         showTooltip();
         updateTooltip(selectedRoom.name, "23", "2000", "offen")
+    }
+    if (intersectedFloors.length > 0 && MODE == "FLOOR"){
+        floorName = intersectedFloors[0].object.name;
+        if(floors.includes(floorName)){
+            await floorSelect(floorName);
+            MODE = "ROOM";
+        }
     }
 }
